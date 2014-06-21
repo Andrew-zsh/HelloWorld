f8540046a3e78997b95075f29155b58a4bcd0b95
huihui2014 <hutu848@163.com>
2014-06-22 00:18:37 +0800

    modify test

--- HelloWord/src/HelloWorld.java
+++ HelloWord/src/HelloWorld.java
@@ -3,6 +3,9 @@ public class HelloWorld {
 	
 	public static void main(String[] args) {
 		System.out.println("Hello World for GitHub.");
+		
+		//Add form local
+		System.out.println("Add from local");
 	}
 
 }
